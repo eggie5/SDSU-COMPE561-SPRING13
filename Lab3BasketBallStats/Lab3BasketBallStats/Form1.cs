9eabe89d4f9571babf4013c05d9a996c3ac649b2
Alex Egg <eggie5@gmail.com>
2013-03-23 17:05:45 -0700

    working finally! hate M$

--- Lab3BasketBallStats/Lab3BasketBallStats/Form1.cs
+++ Lab3BasketBallStats/Lab3BasketBallStats/Form1.cs
@@ -12,7 +12,10 @@ namespace Lab3BasketBallStats
 {
     public partial class Form1 : Form
     {
-        private DataClasses1DataContext database = new DataClasses1DataContext();
+      private StatsDataContext database = new StatsDataContext();
+        
+       
+        private DataGridView current_dg;
       
         public Form1()
         {
@@ -22,8 +25,15 @@ namespace Lab3BasketBallStats
 
         private void leagueBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
+            
+
             Validate();
-            leagueBindingSource.EndEdit();
+
+            current_dg.EndEdit();
+            //leagueBindingSource.EndEdit();
+            Console.WriteLine(current_dg.Name);
+            //teamBindingSource.EndEdit();
+
             database.SubmitChanges();
         }
 
@@ -39,58 +49,41 @@ namespace Lab3BasketBallStats
                 orderby team.Id
                 select team;
 
-            playerBindingSource.DataSource =
-                from player in database.Players
-                orderby player.Id
-                select player;
+            //playerBindingSource.DataSource =
+            //    from player in database.Players
+            //    orderby player.Id
+            //    select player;
 
             gameBindingSource.DataSource =
                 from game in database.Games
                 orderby game.Id
                 select game;
 
-            statisticBindingSource.DataSource =
-                from stat in database.Statistics
-                orderby stat.Id
-                select stat;
+            //statisticBindingSource.DataSource =
+            //    from stat in database.Statistics
+            //    orderby stat.Id
+                //select stat;
            
         }
 
-        private void saveToolStripButton_Click(object sender, EventArgs e)
+        private void teamDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            Validate();
-            teamBindingSource.EndEdit();
-            database.SubmitChanges();
+            this.current_dg=(DataGridView)sender;
+            switch(this.current_dg.Name)
+            {
+                case "leagueDataGridView":
+                    leagueBindingNavigator.BindingSource = leagueBindingSource;
+                    break;
+                case "teamDataGridView":
+                    leagueBindingNavigator.BindingSource = teamBindingSource;
+                    break;
+            }
+            
         }
 
-       
-        private void saveToolStripButton2_Click(object sender, EventArgs e)
-        {
-            Validate();
-            statisticBindingSource.EndEdit();
-            database.SubmitChanges();
-        }
 
-        private void saveToolStripButton3_Click(object sender, EventArgs e)
-        {
-            Validate();
-            gameBindingSource.EndEdit();
-            database.SubmitChanges();
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Players p = new Players();
-            p.db_ref = database;
-            p.Show();
-        }
 
-        private void saveToolStripButton1_Click_1(object sender, EventArgs e)
-        {
-            Validate();
-            playerBindingSource.EndEdit();
-            database.SubmitChanges();
-        }
 
  
     }
